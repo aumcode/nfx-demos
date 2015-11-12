1ba2a199cb3cc701eac9632c36c656875af649f5
sergey-msu <spmathf@gmail.com>
2015-11-12 22:56:03 +0300

    Wave.DbApplication demo: auto schema script generation; base pages

---
+++ Wave/Wave.DbApplication/Controllers/Contacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Wave.DbApplication.Models;
+using NFX.Wave.MVC;
+using Wave.DbApplication.Pages;
+
+namespace Wave.DbApplication.Controllers
+{
+    public class Contacts : Controller
+    {
+        [Action]
+        public object Index()
+        {
+            var persons = new List<Person>
+            {
+                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Sergey", LastName="Polyanskikh", EMail="mathf@rambler.ru" },
+                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Fedor", LastName="Lubenskii", EMail="lubenskiif@mail.ru" },
+                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Grant", LastName="Kesyan", EMail="grant_k@gmail.com" },
+            };
+
+            return new Index { Persons = persons };
+        }
+    }
+}
---
+++ Wave/Wave.DbApplication/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wave.DbApplication.Models
+{
+    public class Person
+    {
+        public string ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public string EMail { get; set; }
+    }
+}
