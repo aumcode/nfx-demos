ffedc09def82a0add3c0e4f832a9e2f4fac08096
sergey-msu <spmathf@gmail.com>
2015-11-13 14:14:31 +0300

    Wave.DbApplication: delete and create operations

--- Wave/Wave.DbApplication/Controllers/Contacts.cs
+++ Wave/Wave.DbApplication/Controllers/Contacts.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Wave.DbApplication.Models;
 using NFX.Wave.MVC;
 using Wave.DbApplication.Pages;
+using NFX;
+using NFX.DataAccess.MySQL;
+using NFX.DataAccess.CRUD;
 
 namespace Wave.DbApplication.Controllers
 {
@@ -11,12 +15,22 @@ namespace Wave.DbApplication.Controllers
         [Action]
         public object Index()
         {
-            var persons = new List<Person>
+            //var persons = new List<Person>
+            //{
+            //    new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Sergey", LastName="Polyanskikh", EMail="mathf@rambler.ru" },
+            //    new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Fedor", LastName="Lubenskii", EMail="lubenskiif@mail.ru" },
+            //    new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Grant", LastName="Kesyan", EMail="grant_k@gmail.com" },
+            //}; 
+
+            var persons = new List<Person>();
+
+            var data = App.DataStore as MySQLDataStore;
+            var query = new Query("Data.Scripts.GetAllPersons", typeof(Person));
+            var set = data.Load(query);
+            if (set != null && set.Any())
             {
-                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Sergey", LastName="Polyanskikh", EMail="mathf@rambler.ru" },
-                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Fedor", LastName="Lubenskii", EMail="lubenskiif@mail.ru" },
-                new Person { ID=Guid.NewGuid().ToString("N"), FirstName="Grant", LastName="Kesyan", EMail="grant_k@gmail.com" },
-            };
+                persons.AddRange(set[0].Cast<Person>());
+            }
 
             return new Index { Persons = persons };
         }
@@ -24,13 +38,29 @@ namespace Wave.DbApplication.Controllers
         [Action]
         public void Delete(string personId)
         {
-
+            var data = App.DataStore as MySQLDataStore;
+            var query = new Query("Data.Scripts.DeletePerson")
+                            {
+                                new Query.Param("id", personId)
+                            };
+            data.ExecuteWithoutFetch(query);
         }
 
         [Action]
         public object Upsert(string personId)
         {
-            var person = new Person { ID = personId };
+            var person = new Person
+            {
+                ID = Guid.NewGuid().ToString("N"),
+                FirstName = "A",
+                MiddleName = "B",
+                LastName = "C",
+                DOB = DateTime.Now,
+                EMail = "abretok@gmail.com"
+            };
+            var data = App.DataStore as MySQLDataStore;
+            data.Insert(person);
+
             return new Edit { Person = person };
         }
     }
--- Wave/Wave.DbApplication/Models/Person.cs
+++ Wave/Wave.DbApplication/Models/Person.cs
@@ -1,19 +1,26 @@
-﻿using System;
+﻿using NFX.DataAccess.CRUD;
+using System;
 
 namespace Wave.DbApplication.Models
 {
-    public class Person
+    public class Person : TypedRow
     {
+        [Field(key: true, required: true)]
         public string ID { get; set; }
 
+        [Field(required: true)]
         public string FirstName { get; set; }
 
+        [Field(required: false)]
         public string MiddleName { get; set; }
 
+        [Field(required: true)]
         public string LastName { get; set; }
 
+        [Field(required: false)]
         public DateTime DOB { get; set; }
 
+        [Field(required: true)]
         public string EMail { get; set; }
     }
 }
